7f3e51852aa2312ca8b7c0d2b7fdebfc4b72a2d2
balasaravanan <33663720+balasaravanan3502@users.noreply.github.com>
2021-07-13 11:44:43 +0530

    Update AuthCard.dart

--- lib/Widgets/AuthCard.dart
+++ lib/Widgets/AuthCard.dart
@@ -17,6 +17,7 @@ class AuthCard extends StatefulWidget {
 }
 
 class _AuthCardState extends State<AuthCard> {
+  bool isLoading = false;
   final TextEditingController _email = TextEditingController();
   final TextEditingController _password = TextEditingController();
   final TextEditingController _confirmpassword = TextEditingController();
@@ -69,196 +70,206 @@ class _AuthCardState extends State<AuthCard> {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: MediaQuery.of(context).size.height * .5,
-      decoration: BoxDecoration(
-        color: Color(0xCCffffff),
-        borderRadius: BorderRadius.only(
-          topLeft: Radius.circular(50),
-          topRight: Radius.circular(50),
-        ),
-      ),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: [
-          SizedBox(
-            height: MediaQuery.of(context).size.height * .01,
-          ),
-          Text(
-            widget.screen,
-            style: TextStyle(
-              color: Color(0xff1A2561),
-              fontSize: 30,
-              fontWeight: FontWeight.bold,
+    return Stack(
+      children: [
+        Container(
+          height: MediaQuery.of(context).size.height * .5,
+          decoration: BoxDecoration(
+            color: Color(0xCCffffff),
+            borderRadius: BorderRadius.only(
+              topLeft: Radius.circular(50),
+              topRight: Radius.circular(50),
             ),
           ),
-          SizedBox(
-            height: MediaQuery.of(context).size.height * .01,
-          ),
-          Padding(
-            padding: EdgeInsets.symmetric(horizontal: 30),
-            child: Column(
-              children: [
-                Container(
-                  decoration: BoxDecoration(
-                    color: Color(0xffAC9E8C),
-                    borderRadius: BorderRadius.all(
-                      Radius.circular(20.0),
-                    ),
-                  ),
-                  child: TextFormField(
-                    controller: _email,
-                    style: TextStyle(color: Colors.white),
-                    decoration: TextFieldDecoration.copyWith(
-                      hintText: 'Email',
-                    ),
-                    cursorColor: Color(0xff2D5C78),
-                    onChanged: (value) {},
-                  ),
-                ),
-                SizedBox(
-                  height: MediaQuery.of(context).size.height * .01,
-                ),
-                Container(
-                  decoration: BoxDecoration(
-                    color: Color(0xffAC9E8C),
-                    borderRadius: BorderRadius.all(
-                      Radius.circular(20.0),
-                    ),
-                  ),
-                  child: TextFormField(
-                    controller: _password,
-                    style: TextStyle(color: Colors.white),
-                    decoration: TextFieldDecoration.copyWith(
-                      hintText: 'Password',
-                    ),
-                    cursorColor: Color(0xff2D5C78),
-                    onChanged: (value) {},
-                  ),
-                ),
-                SizedBox(
-                  height: MediaQuery.of(context).size.height * .01,
-                ),
-                if (widget.screen == 'Sign In')
-                  Align(
-                    alignment: Alignment.topRight,
-                    child: Text(
-                      'Forgot Password?',
-                      style: TextStyle(
-                        color: Color(0xff5D7A9C),
-                        fontWeight: FontWeight.w600,
-                        decoration: TextDecoration.underline,
-                      ),
-                    ),
-                  ),
-                if (widget.screen != 'Sign In')
-                  Container(
-                    decoration: BoxDecoration(
-                      color: Color(0xffAC9E8C),
-                      borderRadius: BorderRadius.all(
-                        Radius.circular(20.0),
-                      ),
-                    ),
-                    child: TextFormField(
-                      controller: _confirmpassword,
-                      style: TextStyle(color: Colors.white),
-                      decoration: TextFieldDecoration.copyWith(
-                        hintText: 'Re-enter Password',
-                      ),
-                      cursorColor: Color(0xff2D5C78),
-                    ),
-                  ),
-              ],
-            ),
-          ),
-          Container(
-            width: MediaQuery.of(context).size.width * .35,
-            child: ElevatedButton(
-              style: ElevatedButton.styleFrom(
-                elevation: 0,
-                shape: StadiumBorder(),
-              ),
-              onPressed: () {
-                _submitted();
-              },
-              child: Padding(
-                padding: const EdgeInsets.all(13.0),
-                child: Text(
-                  widget.screen,
-                  style: TextStyle(
-                    color: Colors.white,
-                  ),
-                ),
-              ),
-            ),
-          ),
-          Column(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Align(
-                    alignment: Alignment.center,
-                    child: Text(
-                      '            ',
-                      style: TextStyle(
-                        color: Colors.grey,
-                        decoration: TextDecoration.lineThrough,
-                        decorationThickness: 2,
-                      ),
-                    ),
-                  ),
-                  SizedBox(
-                    width: 5,
-                  ),
-                  Align(
-                    alignment: Alignment.center,
-                    child: Text('Or ${widget.screen} With'),
-                  ),
-                  SizedBox(
-                    width: 5,
-                  ),
-                  Align(
-                    alignment: Alignment.center,
-                    child: Text(
-                      '            ',
-                      style: TextStyle(
-                        color: Colors.grey,
-                        decoration: TextDecoration.lineThrough,
-                        decorationThickness: 2,
-                      ),
-                    ),
-                  ),
-                ],
+              SizedBox(
+                height: MediaQuery.of(context).size.height * .01,
               ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  OAuth('assets/icons/google.jpg', () {}),
-                  OAuth('assets/icons/facebook.jpg', () {}),
-                ],
+              Text(
+                widget.screen,
+                style: TextStyle(
+                  color: Color(0xff1A2561),
+                  fontSize: 30,
+                  fontWeight: FontWeight.bold,
+                ),
               ),
-              Container(
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
+              SizedBox(
+                height: MediaQuery.of(context).size.height * .01,
+              ),
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 30),
+                child: Column(
                   children: [
-                    Text('Don\'t have an Account?'),
-                    MaterialButton(
-                      onPressed: widget.navigate,
-                      child: Text(
-                        widget.screen != 'Sign In' ? 'Sign In' : 'Sign Up',
-                        style: TextStyle(
-                          color: Colors.orangeAccent,
+                    Container(
+                      decoration: BoxDecoration(
+                        color: Color(0xffAC9E8C),
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(20.0),
                         ),
                       ),
-                    )
+                      child: TextFormField(
+                        controller: _email,
+                        style: TextStyle(color: Colors.white),
+                        decoration: TextFieldDecoration.copyWith(
+                          hintText: 'Email',
+                        ),
+                        cursorColor: Color(0xff2D5C78),
+                        onChanged: (value) {},
+                      ),
+                    ),
+                    SizedBox(
+                      height: MediaQuery.of(context).size.height * .01,
+                    ),
+                    Container(
+                      decoration: BoxDecoration(
+                        color: Color(0xffAC9E8C),
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(20.0),
+                        ),
+                      ),
+                      child: TextFormField(
+                        controller: _password,
+                        style: TextStyle(color: Colors.white),
+                        decoration: TextFieldDecoration.copyWith(
+                          hintText: 'Password',
+                        ),
+                        cursorColor: Color(0xff2D5C78),
+                        onChanged: (value) {},
+                      ),
+                    ),
+                    SizedBox(
+                      height: MediaQuery.of(context).size.height * .01,
+                    ),
+                    if (widget.screen == 'Sign In')
+                      Align(
+                        alignment: Alignment.topRight,
+                        child: Text(
+                          'Forgot Password?',
+                          style: TextStyle(
+                            color: Color(0xff5D7A9C),
+                            fontWeight: FontWeight.w600,
+                            decoration: TextDecoration.underline,
+                          ),
+                        ),
+                      ),
+                    if (widget.screen != 'Sign In')
+                      Container(
+                        decoration: BoxDecoration(
+                          color: Color(0xffAC9E8C),
+                          borderRadius: BorderRadius.all(
+                            Radius.circular(20.0),
+                          ),
+                        ),
+                        child: TextFormField(
+                          controller: _confirmpassword,
+                          style: TextStyle(color: Colors.white),
+                          decoration: TextFieldDecoration.copyWith(
+                            hintText: 'Re-enter Password',
+                          ),
+                          cursorColor: Color(0xff2D5C78),
+                        ),
+                      ),
                   ],
                 ),
               ),
+              Container(
+                width: MediaQuery.of(context).size.width * .35,
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    elevation: 0,
+                    shape: StadiumBorder(),
+                  ),
+                  onPressed: () {
+                    isLoading = true;
+                    _submitted();
+                    isLoading = false;
+                  },
+                  child: Padding(
+                    padding: const EdgeInsets.all(13.0),
+                    child: Text(
+                      widget.screen,
+                      style: TextStyle(
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              Column(
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Align(
+                        alignment: Alignment.center,
+                        child: Text(
+                          '            ',
+                          style: TextStyle(
+                            color: Colors.grey,
+                            decoration: TextDecoration.lineThrough,
+                            decorationThickness: 2,
+                          ),
+                        ),
+                      ),
+                      SizedBox(
+                        width: 5,
+                      ),
+                      Align(
+                        alignment: Alignment.center,
+                        child: Text('Or ${widget.screen} With'),
+                      ),
+                      SizedBox(
+                        width: 5,
+                      ),
+                      Align(
+                        alignment: Alignment.center,
+                        child: Text(
+                          '            ',
+                          style: TextStyle(
+                            color: Colors.grey,
+                            decoration: TextDecoration.lineThrough,
+                            decorationThickness: 2,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      OAuth('assets/icons/google.jpg', () {}),
+                      OAuth('assets/icons/facebook.jpg', () {}),
+                    ],
+                  ),
+                  Container(
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text('Don\'t have an Account?'),
+                        MaterialButton(
+                          onPressed: widget.navigate,
+                          child: Text(
+                            widget.screen != 'Sign In' ? 'Sign In' : 'Sign Up',
+                            style: TextStyle(
+                              color: Colors.orangeAccent,
+                            ),
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                ],
+              ),
             ],
           ),
-        ],
-      ),
+        ),
+        if (isLoading)
+          Center(
+            child: CircularProgressIndicator(),
+          ),
+      ],
     );
   }
 }
